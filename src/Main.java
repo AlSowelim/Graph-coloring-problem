a6b4f273a8bce087d4f27adedd40029ea4c86a6e
Naif Alfayez <naifalfayez4@gmail.com>
2024-03-12 03:16:04 +0300

    ``

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;      //rrrrrrrrrrrrrrrrrrrrrrrrrrr
+import java.util.*;      
 public class Main {
     public static  void main(String[] args) {
         Scanner kb = new Scanner(System.in);
